5a6af2094f5aad47afcc87eba720b14408373438
d-cook <dcook0819@gmail.com>
2016-10-20 01:25:35 -0700

    Creating function to create prototype-less objects

--- Objects.js
+++ Objects.js
@@ -1,5 +1,18 @@
 window.addEventListener("load", function() {
 
+function newObject (keys, obj) {
+   var v = Object.create(null);
+   for (var p in obj) {
+      if (Object.hasOwnProperty.apply(obj, p)) {
+         v[p] = obj[p];
+      }
+   }
+   var o = Object.create(null);
+   o.type = O.types.object;
+   o.value = v;
+   return o;
+}
+
 var O = window.Objects = {};
 
 });
