5568b25b888445171f4ffef4fe47594c58f105dc
d-cook <dcook0819@gmail.com>
2016-10-28 02:17:56 -0700

    Created a "bool" type and rearranged type functions

--- Objects.js
+++ Objects.js
@@ -31,14 +31,24 @@ function run (func) {
 var O = window.Objects = {};
 
 O.types = {};
-O.types.type   = { type: null        , name: 'type'   }; O.isType   = function (o) { return O.typeof(o) === O.types.type;   };
-O.types.object = { type: O.types.type, name: 'object' }; O.isObject = function (o) { return O.typeof(o) === O.types.object; };
-O.types.string = { type: O.types.type, name: 'string' }; O.isString = function (o) { return O.typeof(o) === O.types.string; };
-O.types.array  = { type: O.types.type, name: 'array'  }; O.isArray  = function (o) { return O.typeof(o) === O.types.array;  };
-O.types.null   = { type: O.types.type, name: 'null'   }; O.isNull   = function (o) { return O.typeof(o) === O.types.null;   };
-O.types.number = { type: O.types.type, name: 'number' }; O.isNumber = function (o) { return O.typeof(o) === O.types.number; };
-O.types.native = { type: O.types.type, name: 'native' }; O.isNative = function (o) { return O.typeof(o) === O.types.native; };
+O.types.type   = { type: null        , name: 'type'   };
+O.types.null   = { type: O.types.type, name: 'null'   };
+O.types.bool   = { type: O.types.type, name: 'bool'   };
+O.types.number = { type: O.types.type, name: 'number' };
+O.types.string = { type: O.types.type, name: 'string' };
+O.types.array  = { type: O.types.type, name: 'array'  };
+O.types.object = { type: O.types.type, name: 'object' };
+O.types.native = { type: O.types.type, name: 'native' };
 O.types.type.type = O.types.type;
+   
+O.isType   = function (o) { return O.typeof(o) === O.types.type;   };
+O.isNull   = function (o) { return O.typeof(o) === O.types.null;   };
+O.isBool   = function (o) { return O.typeof(o) === O.types.bool;   };
+O.isNumber = function (o) { return O.typeof(o) === O.types.number; };
+O.isString = function (o) { return O.typeof(o) === O.types.string; };
+O.isArray  = function (o) { return O.typeof(o) === O.types.array;  };
+O.isObject = function (o) { return O.typeof(o) === O.types.object; };
+O.isNative = function (o) { return O.typeof(o) === O.types.native; };
 
 O.typeof = function (obj) { return (obj && obj.type) || O.types.null; };
 
