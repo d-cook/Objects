d407f885c02a8e1dc1ae5127cfa391e9125d19f6
d-cook <dcook0819@gmail.com>
2016-11-05 03:35:09 -0700

    Added missing arg for tryGet(): [op] -> [op, env]

--- Objects.js
+++ Objects.js
@@ -112,7 +112,7 @@ O.eval = function (cb, code, env) {
          return tailcall(cb, [code]);
       }
       // Execute an operation defined in env
-      return tailcall(O.tryGet, ['op'], function (noCb, hasOp, op) {
+      return tailcall(O.tryGet, ['op', env], function (noCb, hasOp, op) {
          // If there is no operation to perform, just return it as a value
          if (!hasOp) {
             return tailcall(cb, [code]);
